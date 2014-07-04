90ecc2e94888864f21467074c4c45e9b8f3dcdc7
mxro <mxro@nowhere.com>
2014-07-04 17:55:32 +1200

    Refactoring Appjangle Server (nxAppCloudNode) to make it more portable and usable for tests Autocommit Fri Jul 04 17:55:32 NZST 2014

--- src/main/java/io/nextweb/persistence/js/JsSerializer.java
+++ src/main/java/io/nextweb/persistence/js/JsSerializer.java
@@ -6,8 +6,6 @@ import nx.serializer.utils.StringSource;
 
 public interface JsSerializer extends Serializer<StringSource, StringDestination> {
 
-	//public String serialize(Object obj);
 
-	//public Object deserialize(String data);
 
 }
